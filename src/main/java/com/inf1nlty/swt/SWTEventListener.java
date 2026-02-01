b0b66cbccc84adb7c31875478e5e8b02a7054742
Inf1nity <1449896523@qq.com>
2026-02-01 21:23:22 +0800

    Change clain logic into login event

--- src/main/java/com/inf1nlty/swt/SWTEventListener.java
+++ src/main/java/com/inf1nlty/swt/SWTEventListener.java
@@ -1,13 +1,36 @@
 package com.inf1nlty.swt;
 
+import net.minecraft.*;
 import com.google.common.eventbus.Subscribe;
-import net.xiaoyu233.fml.reload.event.CommandRegisterEvent;
-import com.inf1nlty.swt.command.CommandClaimTorch;
+import net.xiaoyu233.fml.reload.event.PlayerLoggedInEvent;
 
 public class SWTEventListener {
 
     @Subscribe
-    public static void register(CommandRegisterEvent event) {
-        event.register(new CommandClaimTorch());
+    public static void onPlayerLoggedIn(PlayerLoggedInEvent event) {
+
+        ServerPlayer player = event.getPlayer();
+        if (player == null) return;
+
+        IClaim claim = (IClaim) (Object) player;
+
+        if (claim.swt_hasClaimedTorch()) {
+            return;
+        }
+
+        World world = player.worldObj;
+        ItemStack torchStack = new ItemStack(Block.torchWood, 1, 0);
+        boolean added = player.inventory.addItemStackToInventory(torchStack);
+
+        if (!added) {
+            EntityItem ei = new EntityItem(world, player.posX, player.posY + 1.0D, player.posZ, torchStack);
+            world.spawnEntityInWorld(ei);
+        }
+
+        claim.swt_setClaimedTorch(true);
+
+        player.sendChatToPlayer(ChatMessageComponent.createFromTranslationKey("swt.claim.success").setColor(EnumChatFormatting.GREEN)
+        );
     }
+
 }
